eac1987c1d8a2b565b3d6004dba16c821e962095
rafaellimareis0 <rafaellimareis0@gmail.com>
2024-02-01 21:06:55 -0300

    Chat em tempo real - Aula 4

--- Aula4 - Chat em Tempo Real/server.js
+++ Aula4 - Chat em Tempo Real/server.js
@@ -14,4 +14,15 @@ io.addListener("connection", (socket) => {
   });
 });
 
-servidorHTTP.listen(4040);
+servidorHTTP.listen(4040, '192.168.1.4');
+
+
+
+//Para inicio tem que instalar os pacotes do nodejs na sequencia abaixo:
+//npm init -y
+//npm install express
+//npm install socket.io
+
+//Para rodar ao servidor, só alterar o IP da linha 17 para o IPV4 do computador e se necessário a porta.
+
+//Após alterar o IPV4, rodar o comando >> node --watch ./server.js OU node ./server.js
